9d67b3d839d1b5ea55231798d64e0ca473db9f9a
scarlet0725 <20381860+scarlet0725@users.noreply.github.com>
2022-11-08 02:53:51 +0900

    :Fix json tag

--- model/model.go
+++ model/model.go
@@ -397,7 +397,7 @@ type Event struct {
 	ID        string    `json:"id"`
 	Name      string    `json:"name"`
 	Date      time.Time `json:"date"`
-	OpenTime  time.Time `json:"open_date"`
+	OpenTime  time.Time `json:"open_time"`
 	StartTime time.Time `json:"start_time"`
 	EndTime   time.Time `json:"end_time"`
 	Location  string    `json:"location"`
