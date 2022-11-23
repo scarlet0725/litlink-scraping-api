435ee4b23f8a6ee989b1e6134436531c0267cd3d
scarlet0725 <20381860+scarlet0725@users.noreply.github.com>
2022-11-24 01:22:27 +0900

    会場関係を追加

--- model/venue.go
+++ model/venue.go
@@ -5,8 +5,10 @@ type Venue struct {
 	VenueID     string `json:"venue_id" gorm:"unique;not null"`
 	Name        string `json:"name" gorm:"not null"`
 	Description string `json:"description"`
+	WebSite     string `json:"website"`
 	Postcode    string `json:"post_code"`
 	Prefecture  string `json:"prefecture"`
 	City        string `json:"city"`
 	Street      string `json:"street"`
+	IsOpen      bool   `json:"is_open" gorm:"not null;default:true"`
 }
