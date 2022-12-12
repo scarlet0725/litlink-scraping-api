184cc13ba3d0b46e3180b6dda7ef68aed3f25ace
scarlet0725 <20381860+scarlet0725@users.noreply.github.com>
2022-12-12 18:15:34 +0900

    ファイルの分離

---
+++ infrastructure/artist.go
@@ -0,0 +1,65 @@
+package infrastructure
+
+import (
+	"github.com/scarlet0725/prism-api/infrastructure/repository"
+	"github.com/scarlet0725/prism-api/model"
+	"github.com/scarlet0725/prism-api/schema"
+	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
+)
+
+type artist struct {
+	db *gorm.DB
+}
+
+func NewArtistRepository(db *gorm.DB) repository.ArtistRepository {
+	return &artist{
+		db: db,
+	}
+}
+
+func (a *artist) CreateArtist(artist *model.Artist) (*model.Artist, error) {
+	var schema schema.Artist
+	schema.Artist = *artist
+	err := a.db.Create(&artist).Error
+	if err != nil {
+		return nil, err
+	}
+	return artist, nil
+}
+
+func (a *artist) GetArtistByName(name string) (*model.Artist, error) {
+	var artist model.Artist
+	err := a.db.Preload(clause.Associations).Where("name = ?", name).First(&artist).Error
+	if err != nil {
+		return nil, err
+	}
+	return &artist, nil
+}
+
+func (a *artist) GetArtistByID(id string) (*model.Artist, error) {
+	var artist model.Artist
+	err := a.db.Where("artist_id = ?", id).First(&artist).Error
+	if err != nil {
+		return nil, err
+	}
+	return &artist, nil
+}
+
+func (a *artist) GetArtistsByIDs(ids []string) ([]*model.Artist, error) {
+	var artists []*model.Artist
+	err := a.db.Where("artist_id IN ?", ids).Find(&artists).Error
+	if err != nil {
+		return nil, err
+	}
+	return artists, nil
+}
+
+func (a *artist) GetEventsByID(ID string) (*model.Event, error) {
+	var event *model.Event
+	err := a.db.Preload(clause.Associations).Where("event_id = ?", ID).First(&event).Error
+	if err != nil {
+		return nil, err
+	}
+	return event, nil
+}
