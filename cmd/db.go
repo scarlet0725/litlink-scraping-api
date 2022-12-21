e8f3dcf6af4fa4066a5a7f69482136c23e373f6e
scarlet0725 <20381860+scarlet0725@users.noreply.github.com>
2022-12-21 16:06:55 +0900

    Revert "Merge pull request #42 from scarlet0725/fix/db-connection"
    This reverts commit 5188a9352fdb0e947e24154af2ad40346fc70000, reversing
    changes made to ba95be683bcae289ea985e40bdac10bd42f98a26.

--- cmd/db.go
+++ cmd/db.go
@@ -1,27 +1,23 @@
 package cmd
 
 import (
-	"errors"
-	"math"
+	"log"
 	"time"
-
-	"github.com/scarlet0725/prism-api/logger"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func ConnectDB(dsn string) (*gorm.DB, error) {
-	for i := 0; ; i++ {
-		db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-		if err == nil {
-			logger.Info("database connected")
-			return db, nil
+	c := 0
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		time.Sleep(time.Second)
+		c += 1
+		if c > 30 {
+			log.Fatal(err)
 		}
-		if i > 10 {
-			return nil, errors.New("failed to connect database")
-		}
-		logger.Warn("failed to connect database, retrying...")
-		time.Sleep(time.Duration(math.Pow(2, float64(i))) * time.Second)
+
 	}
+	return db, nil
 }
