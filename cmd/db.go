8fc2faab6b5d428ac924f256741957179a3c5652
scarlet0725 <20381860+scarlet0725@users.noreply.github.com>
2022-12-21 10:22:19 +0900

    DBの接続をリトライするようにした。

--- cmd/db.go
+++ cmd/db.go
@@ -1,23 +1,27 @@
 package cmd
 
 import (
-	"log"
+	"errors"
+	"math"
 	"time"
+
+	"github.com/scarlet0725/prism-api/logger"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func ConnectDB(dsn string) (*gorm.DB, error) {
-	c := 0
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	if err != nil {
-		time.Sleep(time.Second)
-		c += 1
-		if c > 30 {
-			log.Fatal(err)
+	for i := 0; ; i++ {
+		db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+		if err == nil {
+			logger.Info("database connected")
+			return db, nil
 		}
-
+		if i > 10 {
+			return nil, errors.New("failed to connect database")
+		}
+		logger.Warn("failed to connect database, retrying...")
+		time.Sleep(time.Duration(math.Pow(2, float64(i))) * time.Second)
 	}
-	return db, nil
 }
