e51891c323f924a8f45174e35a085395ecfef3d5
scarlet0725 <20381860+scarlet0725@users.noreply.github.com>
2022-12-21 16:08:21 +0900

    Remove Old Logging

--- cmd/db.go
+++ cmd/db.go
@@ -5,8 +5,6 @@ import (
 	"math"
 	"time"
 
-	"github.com/scarlet0725/prism-api/logger"
-
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -15,13 +13,11 @@ func ConnectDB(dsn string) (*gorm.DB, error) {
 	for i := 0; ; i++ {
 		db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 		if err == nil {
-			logger.Info("database connected")
 			return db, nil
 		}
 		if i > 10 {
 			return nil, errors.New("failed to connect database")
 		}
-		logger.Warn("failed to connect database, retrying...")
 		time.Sleep(time.Duration(math.Pow(2, float64(i))) * time.Second)
 	}
 }
