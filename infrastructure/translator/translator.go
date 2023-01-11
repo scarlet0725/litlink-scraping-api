5d16a381253c4c6aebe2b0f8e212aa10093e4dd8
scarlet0725 <20381860+scarlet0725@users.noreply.github.com>
2023-01-12 07:59:53 +0900

    feat: ent.User から model.Userの変換関数を実装

--- infrastructure/translator/translator.go
+++ infrastructure/translator/translator.go
@@ -1,6 +1,8 @@
 package translator
 
 import (
+	"time"
+
 	"github.com/scarlet0725/prism-api/ent"
 	"github.com/scarlet0725/prism-api/model"
 )
@@ -14,4 +16,29 @@ func ArtistFromEnt(source *ent.Artist) *model.Artist {
 	}
 
 	return artist
+}
+
+func UserFromEnt(source *ent.User) *model.User {
+	user := &model.User{
+		ID:               uint(source.ID),
+		UserID:           source.UserID,
+		Username:         source.Username,
+		FamilyName:       source.FirstName,
+		GivenName:        source.LastName,
+		Email:            source.Email,
+		Password:         source.Password,
+		APIKey:           source.APIKey,
+		IsAdminVerified:  source.IsAdminVerified,
+		DeleteProtected:  source.DeleteProtected,
+		CreatedAt:        time.Time{},
+		UpdatedAt:        time.Time{},
+		DeletedAt:        &time.Time{},
+		Events:           []*model.Event{},
+		Roles:            []*model.Role{},
+		GoogleToken:      &model.GoogleOAuthToken{},
+		GoogleOAuthState: &model.GoogleOAuthState{},
+		ExternalCalendar: &model.ExternalCalendar{},
+	}
+
+	return user
 }
