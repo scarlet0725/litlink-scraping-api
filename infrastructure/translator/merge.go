44a3a7b4d4536b828dcf13c6293505f336fa500e
scarlet0725 <20381860+scarlet0725@users.noreply.github.com>
2023-01-16 20:09:38 +0900

    fix: Bug fix

--- infrastructure/translator/merge.go
+++ infrastructure/translator/merge.go
@@ -5,10 +5,22 @@ import (
 	"github.com/scarlet0725/prism-api/model"
 )
 
-func MergeUser(base, patch *model.User) error {
-	err := mergo.Merge(base, *patch)
+func MergeUser(base, patch *model.User) (*model.User, error) {
+	baseUser := *base
+	patchData := *patch
+	err := mergo.Merge(&patchData, baseUser)
 	if err != nil {
-		return err
+		return nil, err
 	}
-	return nil
+
+	//mergoの仕様上ゼロ値を上書きしないのでture -> falseの場合はfalseにする
+	if !baseUser.IsAdminVerified && patchData.IsAdminVerified {
+		patchData.IsAdminVerified = true
+	}
+
+	if !baseUser.DeleteProtected && patchData.DeleteProtected {
+		patchData.DeleteProtected = true
+	}
+
+	return &patchData, nil
 }
