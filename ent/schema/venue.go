68449b0965ce50854be0f2b29af07376031b32af
scarlet0725 <20381860+scarlet0725@users.noreply.github.com>
2023-01-24 15:17:25 +0900

    feat(ent): schemaの追加

--- ent/schema/venue.go
+++ ent/schema/venue.go
@@ -9,43 +9,34 @@ import (
 	"entgo.io/ent/schema"
 	"entgo.io/ent/schema/edge"
 	"entgo.io/ent/schema/field"
-	"entgo.io/ent/schema/index"
 )
 
-type Event struct {
+type Venue struct {
 	ent.Schema
 }
 
-func (Event) Annotations() []schema.Annotation {
+func (Venue) Annotations() []schema.Annotation {
 	return []schema.Annotation{
 		entsql.Annotation{
-			Table:   "events",
+			Table:   "venues",
 			Charset: "utf8mb4",
 		},
 	}
 }
 
-func (Event) Indexes() []ent.Index {
-	return []ent.Index{
-		index.Fields("event_id").
-			Unique(),
-		index.Fields("date", "open_time", "start_time", "end_time"), //TODO: ElasticSearchに切り替えたらインデックスを外す
-	}
-}
-
-func (Event) Fields() []ent.Field {
+func (Venue) Fields() []ent.Field {
 	return []ent.Field{
-		field.String("event_id").
+		field.String("venue_id").
 			Unique().
 			NotEmpty().
+			SchemaType(
+				map[string]string{
+					dialect.MySQL: "varchar(32)",
+				},
+			).
 			Annotations(
 				entsql.Annotation{
 					Collation: "utf8mb4_0900_ai_ci",
-				},
-			).
-			SchemaType(
-				map[string]string{
-					dialect.MySQL: "varchar(191)",
 				},
 			),
 		field.String("name").
@@ -53,43 +44,6 @@ func (Event) Fields() []ent.Field {
 			Annotations(
 				entsql.Annotation{
 					Collation: "utf8mb4_ja_0900_as_cs_ks",
-				},
-			).
-			SchemaType(
-				map[string]string{
-					dialect.MySQL: "longtext",
-				},
-			),
-		field.Time("date").
-			Optional().
-			Nillable().
-			SchemaType(
-				map[string]string{
-					dialect.MySQL: "datetime(3)",
-				},
-			),
-		field.Time("open_time").
-			Optional().
-			Nillable().
-			SchemaType(
-				map[string]string{
-					dialect.MySQL: "datetime(3)",
-				},
-			),
-		field.Time("start_time").
-			Optional().
-			Nillable().
-			SchemaType(
-				map[string]string{
-					dialect.MySQL: "datetime(3)",
-				},
-			),
-		field.Time("end_time").
-			Optional().
-			Nillable().
-			SchemaType(
-				map[string]string{
-					dialect.MySQL: "datetime(3)",
 				},
 			),
 		field.String("description").
@@ -104,11 +58,30 @@ func (Event) Fields() []ent.Field {
 					dialect.MySQL: "longtext",
 				},
 			),
-		field.String("url").
+		field.Bytes("web_site").
+			Optional().
+			SchemaType(
+				map[string]string{
+					dialect.MySQL: "longtext",
+				},
+			),
+		field.String("postcode").
 			Optional().
 			Annotations(
 				entsql.Annotation{
-					Collation: "utf8mb4_bin",
+					Collation: "utf8mb4_ja_0900_as_cs_ks",
+				},
+			).
+			SchemaType(
+				map[string]string{
+					dialect.MySQL: "varchar(8)",
+				},
+			),
+		field.String("prefecture").
+			Optional().
+			Annotations(
+				entsql.Annotation{
+					Collation: "utf8mb4_ja_0900_as_cs_ks",
 				},
 			).
 			SchemaType(
@@ -116,11 +89,11 @@ func (Event) Fields() []ent.Field {
 					dialect.MySQL: "longtext",
 				},
 			),
-		field.String("ticket_url").
+		field.String("city").
 			Optional().
 			Annotations(
 				entsql.Annotation{
-					Collation: "utf8mb4_bin",
+					Collation: "utf8mb4_ja_0900_as_cs_ks",
 				},
 			).
 			SchemaType(
@@ -128,6 +101,20 @@ func (Event) Fields() []ent.Field {
 					dialect.MySQL: "longtext",
 				},
 			),
+		field.String("street").
+			Optional().
+			Annotations(
+				entsql.Annotation{
+					Collation: "utf8mb4_ja_0900_as_cs_ks",
+				},
+			).
+			SchemaType(
+				map[string]string{
+					dialect.MySQL: "longtext",
+				},
+			),
+		field.Bool("is_open").
+			Default(true),
 		field.Time("created_at").
 			Immutable().
 			Default(time.Now).
@@ -155,13 +142,9 @@ func (Event) Fields() []ent.Field {
 	}
 }
 
-func (Event) Edges() []ent.Edge {
+func (Venue) Edges() []ent.Edge {
 	return []ent.Edge{
-		edge.From("users", User.Type).
-			Ref("events"),
-		edge.To("artists", Artist.Type),
-		edge.To("related_ryzm_events", RyzmEvent.Type),
-		edge.To("un_structured_event_informations", UnStructuredEventInformation.Type),
-		edge.To("venue", Venue.Type).Unique(),
+		edge.From("events", Event.Type).
+			Ref("venue"),
 	}
 }
